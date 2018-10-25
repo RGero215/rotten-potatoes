e1dfdbb5bee979c889c0b5d4eb9ed27306f7a238
Ramon Geronimo <rgero215@gmail.com>
2018-10-24 20:02:33 -0700

    Added MongoDB and Review model

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/rotten-potatoes');
+
+const Review = mongoose.model('Review', {
+    title: String,
+    movieTitle: String
+})
+
 
 var exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
@@ -12,16 +20,23 @@ app.get('/', (req, res) => {
 })
 
 // OUR MOCK ARRAY OF PROJECTS
-let reviews = [
-    {title: 'Great Review', movieTitle: 'Batman II'},
-    {title: 'Awesome Movie', movieTitle: 'Titanic'},
-    {title: 'Great Movie', movieTitle: 'Peaceful Warrior'},
-    {title: 'The Movie is OK', movieTitle: 'Venon'},
-]
+// let reviews = [
+//     {title: 'Great Review', movieTitle: 'Batman II'},
+//     {title: 'Awesome Movie', movieTitle: 'Titanic'},
+//     {title: 'Great Movie', movieTitle: 'Peaceful Warrior'},
+//     {title: 'The Movie is OK', movieTitle: 'Venon'},
+// ]
 
 // INDEX
 app.get('/reviews', (req, res) => {
-    res.render('reviews-index', {reviews: reviews});
+    Review.find()
+        .then(reviews => {
+            res.render('reviews-index', {reviews: reviews});
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    
 })
 
 app.listen(3000, () => {
