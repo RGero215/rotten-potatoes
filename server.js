4e506af79760f98c1ebea3df24cad3d28ba5ec8a
Ramon Geronimo <rgero215@gmail.com>
2018-10-24 20:19:08 -0700

    User can create reviews

--- server.js
+++ server.js
@@ -5,13 +5,18 @@ mongoose.connect('mongodb://localhost/rotten-potatoes');
 
 const Review = mongoose.model('Review', {
     title: String,
+    description: String,
     movieTitle: String
 })
-
+// INITIALIZE BODY-PARSER AND ADD IT TO APP
+const bodyParser = require('body-parser')
 
 var exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+
+// The following line must appear AFTER const app = express() and before your routes!
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 
@@ -37,6 +42,22 @@ app.get('/reviews', (req, res) => {
             console.log(err);
         })
     
+})
+
+// NEW
+app.get('/reviews/new', (req, res) => {
+    res.render('reviews-new', {});
+})
+
+// CREATE
+app.post('/reviews', (req, res) => {
+    Review.create(req.body).then((review) => {
+        console.log(review);
+        res.redirect('/');
+    }).catch((err) => {
+        console.log(err.message);
+    })
+
 })
 
 app.listen(3000, () => {
