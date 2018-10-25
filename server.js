0b7e4783b8e5f99e1cc0f96612420f5487060a5e
Ramon Geronimo <rgero215@gmail.com>
2018-10-25 08:39:17 -0700

    Refactor and add test

--- server.js
+++ server.js
@@ -2,18 +2,13 @@ const express = require('express')
 const app = express()
 const methodOverride = require('method-override')
 
-const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost/rotten-potatoes');
 
-const Review = mongoose.model('Review', {
-    title: String,
-    description: String,
-    movieTitle: String,
-    rating: Number
-})
+
 // INITIALIZE BODY-PARSER AND ADD IT TO APP
 const bodyParser = require('body-parser')
+
+const reviews = require('./controllers/reviews')(app);
 
 var exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
@@ -25,81 +20,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride('_method'))
 
 
-
-
-// OUR MOCK ARRAY OF PROJECTS
-// let reviews = [
-//     {title: 'Great Review', movieTitle: 'Batman II'},
-//     {title: 'Awesome Movie', movieTitle: 'Titanic'},
-//     {title: 'Great Movie', movieTitle: 'Peaceful Warrior'},
-//     {title: 'The Movie is OK', movieTitle: 'Venon'},
-// ]
-
-// INDEX
-app.get('/', (req, res) => {
-    Review.find()
-        .then(reviews => {
-            res.render('reviews-index', {reviews: reviews});
-        })
-        .catch(err => {
-            console.log(err);
-        })
-    
-})
-
-// NEW
-app.get('/reviews/new', (req, res) => {
-    res.render('reviews-new', {});
-})
-
-// CREATE
-app.post('/reviews', (req, res) => {
-    Review.create(req.body).then((review) => {
-        console.log(review);
-        res.redirect('/');
-    }).catch((err) => {
-        console.log(err.message);
+if(!module.parent) {
+    app.listen(3000, () => {
+        console.log('App listening on port 3000!')
     })
-
-})
-
-// SHOW
-app.get('/reviews/:id', (req, res) => {
-    Review.findById(req.params.id).then((review) =>{
-        res.render('reviews-show', {review: review})
-    }).catch((err) => {
-        console.log(err.message);
-    })
-})
-
-// EDIT
-app.get('/reviews/:id/edit', (req, res) => {
-    Review.findById(req.params.id, function(err, review) {
-      res.render('reviews-edit', {review: review});
-    })
-})
-
-// UPDATE
-app.put('/reviews/:id', (req, res) => {
-    Review.findByIdAndUpdate(req.params.id, req.body)
-      .then(review => {
-        res.redirect(`/reviews/${review._id}`)
-      })
-      .catch(err => {
-        console.log(err.message)
-      })
-})
-
-// DELETE
-app.delete('/reviews/:id', function (req, res) {
-    console.log("DELETE review")
-    Review.findByIdAndRemove(req.params.id).then((review) => {
-      res.redirect('/');
-    }).catch((err) => {
-      console.log(err.message);
-    })
-})
-
-app.listen(3000, () => {
-  console.log('App listening on port 3000!')
-})
+ }
+module.exports = app;
