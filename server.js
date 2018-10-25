dade066d931e8ecba6557de27160ce6581af448d
Ramon Geronimo <rgero215@gmail.com>
2018-10-24 19:41:10 -0700

    Users can see all reviews

--- server.js
+++ server.js
@@ -8,7 +8,18 @@ app.set('view engine', 'handlebars');
 
 
 app.get('/', (req, res) => {
-  res.render('home', {msg: 'Handlebars are Cool!'})
+  res.render('reviews-index', {reviews: reviews})
+})
+
+// OUR MOCK ARRAY OF PROJECTS
+let reviews = [
+    {title: 'Great Review', movieTitle: 'Batman II'},
+    {title: 'Awesome Movie', movieTitle: 'Titanic'}
+]
+
+// INDEX
+app.get('/reviews', (req, res) => {
+    res.render('reviews-index', {reviews: reviews});
 })
 
 app.listen(3000, () => {
